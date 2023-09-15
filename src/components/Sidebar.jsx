e27da82f432de25ca9e9b39e3db49a5b96c2df08
Cris <ghinghiloschicristina@gmail.com>
2023-09-15 10:48:16 +0200

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import logo from '../assets/img/logo.png';
 import PropTypes from 'prop-types';
 
 export const Sidebar = ({ onSelect }) => {
@@ -20,7 +21,10 @@ export const Sidebar = ({ onSelect }) => {
     return (
         <div className="w-72 flex overflow-hidden">
             <div className="w-full h-auto bg-blue-900 dark:bg-sky-950 m-2.5 p-6 rounded shadow-md shadow-blue-950 dark:shadow-slate-950">
-                <h2 className="text-3xl font-bold mb-8 text-white">Climatic</h2>
+                <div className="flex items-center mb-8"> {/* Flex container */}
+                    <img src={logo} alt="logo" className='mr-2 w-12 h-12' /> {/* Adjust size with w-12 and h-12, adjust spacing with mr-4 */}
+                    <h2 className="text-3xl font-bold text-white">Climatic</h2>
+                </div>
                 <ul className="space-y-4">
                     {links.map(link => (
                         <li key={link.path}>
