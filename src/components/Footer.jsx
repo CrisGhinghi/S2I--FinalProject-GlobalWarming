a6ea5cee2eb3cb30086487c84058ca74c8e0a1a2
Cris <ghinghiloschicristina@gmail.com>
2023-09-15 09:39:57 +0200

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+export const Footer = () => {
+    return (
+        <footer className="text-center text-xs py-2 h-8">
+            Created by Cristina Ghinghiloschi © 2023
+        </footer>
+    );
+}
