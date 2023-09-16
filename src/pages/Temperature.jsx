c8ac64e431023b6dc6d40a0b4fe15ce4823e04e6
Cris <ghinghiloschicristina@gmail.com>
2023-09-16 09:49:37 +0200

    Update Temperature.jsx

--- src/pages/Temperature.jsx
+++ src/pages/Temperature.jsx
@@ -29,7 +29,7 @@ const updateTemperatureData = (prevData, data) => ({
 
 const Impacts = () => (
   <div>
-    <h2 className='text-lg md:text-xl font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Impacts</h2>
+    <h2 className='text-lg font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Impacts</h2>
     <ul className='list-none'>
       <li className='mb-1'><FontAwesomeIcon icon={faExclamationTriangle} className='mr-2 text-sm' />Extinction of animal and plant species</li>
       <li className='mb-1'><FontAwesomeIcon icon={faWater} className='mr-2 text-sm' />Reduction of water resources</li>
@@ -41,7 +41,7 @@ const Impacts = () => (
 
 const Solutions = () => (
   <div>
-    <h2 className='text-lg md:text-xl font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Solutions</h2>
+    <h2 className='text-lg font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Solutions</h2>
     <ul className='list-none'>
       <li className='mb-1'><FontAwesomeIcon icon={faWind} className='mr-2 text-sm' />Reduction of greenhouse gas emissions</li>
       <li className='mb-1'><FontAwesomeIcon icon={faSun} className='mr-2 text-sm' />Use of renewable energies such as solar and wind</li>
@@ -80,19 +80,26 @@ export const Temperature = () => {
 
   return (
     <div className="md:h-[calc(100vh-56px-2rem)] flex flex-col items-center bg-sky-100 dark:bg-sky-900 rounded p-6 ml-2 mr-2 md:mr-4 shadow-md dark:shadow-slate-900 text-sm md:text-base">
-      <h1 className="text-4xl md:text-5xl font-bold mb-5 text-blue-900 dark:text-cyan-400">Temperature</h1>
+      <h1 className="text-4xl font-bold mb-4 text-blue-900 dark:text-cyan-400">Temperature</h1>
+
       <div className="prose prose-lg max-w-screen-lg">
-        <p>
-          Global warming is one of the main challenges of our time. It refers to the increase in average temperatures of the Earth{"'"}s surface, oceans, and atmosphere, mainly caused by human activities, such as the burning of fossil fuels, deforestation, and agriculture.
-        </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+        <p>Global warming is one of the major challenges of our time.</p>
+
+        <p className='my-2'>It refers to the rise in average temperatures of the Earth{"'"}s surface, oceans, and atmosphere.</p>
+
+        <p className='mb-2'>It is caused by human activities that result in the release of gases into the atmosphere which, although also present naturally, end up increasing their density, disrupting the amount of energy present on Earth and raising its global temperature.</p>
+
+        <p>Human activities that cause global warming include the burning of fossil fuels, deforestation, and agriculture.</p>
+        
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
           <Impacts />
           <Solutions />
         </div>
       </div>
-      <div className="bg-white dark:bg-gray-100 rounded-lg shadow-lg w-full max-w-4xl mt-10 p-5">
+
+      <div className="bg-white dark:bg-gray-100 rounded-lg shadow-lg w-full max-w-4xl mt-4 p-3">
         <BarChart chartData={temperatureData} />
       </div>
-    </div>
+    </div >
   );
 }
