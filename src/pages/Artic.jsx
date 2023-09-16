d8db76a02bd401737a2293ec5002baac3dcd5fe3
Cris <ghinghiloschicristina@gmail.com>
2023-09-16 09:49:45 +0200

    Update Artic.jsx

--- src/pages/Artic.jsx
+++ src/pages/Artic.jsx
@@ -29,7 +29,7 @@ const updateArticData = (prevData, data) => ({
 
 const Impacts = () => (
   <div>
-    <h2 className='text-lg md:text-xl font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Impacts</h2>
+    <h2 className='text-lg font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Impacts</h2>
     <ul className='list-none'>
       <li className='mb-1'><FontAwesomeIcon icon={faWater} className='mr-2 text-sm' />Sea level rise</li>
       <li className='mb-1'><FontAwesomeIcon icon={faPaw} className='mr-2 text-sm' />Loss of habitat for polar species</li>
@@ -41,7 +41,7 @@ const Impacts = () => (
 
 const Solutions = () => (
   <div>
-    <h2 className='text-lg md:text-xl font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Solutions</h2>
+    <h2 className='text-lg font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Solutions</h2>
     <ul className='list-none'>
       <li className='mb-1'><FontAwesomeIcon icon={faWind} className='mr-2 text-sm' />Limit greenhouse gas emissions to slow down global warming</li>
       <li className='mb-1'><FontAwesomeIcon icon={faSnowflake} className='mr-2 text-sm' />Protect and conserve polar habitats</li>
@@ -86,17 +86,28 @@ export const Artic = () => {
 
   return (
     <div className="md:h-[calc(100vh-56px-2rem)] flex flex-col items-center bg-sky-100 dark:bg-sky-900 rounded p-6 ml-2 mr-2 md:mr-4 shadow-md dark:shadow-slate-900 text-sm md:text-base">
-      <h1 className="text-4xl md:text-5xl font-bold mb-5 text-blue-900 dark:text-cyan-400">Artic</h1>
-      <ul className="prose max-w-screen-lg">
+      <h1 className="text-4xl font-bold mb-4 text-blue-900 dark:text-cyan-400">Artic</h1>
+
+      <div className="prose max-w-screen-lg">
         <p>
-          The polar ice, found both in the Arctic and Antarctic, is a key indicator of climate change. Its melting accelerates global warming since ice reflects sunlight, while water absorbs it. The loss of polar ice has severe repercussions not only on the environment but also on animal species and human communities.
+          The melting of glaciers, evident in both the Arctic and Antarctic, is a key indicator of climate change.
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+
+        <p className='my-2'>
+          They are a reservoir of water of immeasurable value and serve a protective covering function for the Earth and the oceans. Moreover, due to their reflective effect, they disperse excess heat into space and keep the planet cooler.
+        </p>
+
+        <p>
+          The loss of polar ice has severe repercussions not only on the environment but also on animal species and human communities.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
           <Impacts />
           <Solutions />
         </div>
-      </ul>
-      <div className="bg-white dark:bg-gray-100 rounded-md shadow-md w-full max-w-4xl mt-5 p-5">
+      </div>
+
+      <div className="bg-white dark:bg-gray-100 rounded-md shadow-md w-full max-w-4xl mt-4 p-3">
         <LineChart chartData={articData} />
       </div>
     </div>
