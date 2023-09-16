e263cdf1ce4aeaddfb4df97caa411253cc54d51e
Cris <ghinghiloschicristina@gmail.com>
2023-09-16 09:49:41 +0200

    Update Methane.jsx

--- src/pages/Methane.jsx
+++ src/pages/Methane.jsx
@@ -29,7 +29,7 @@ const updateMethaneData = (prevData, data) => ({
 
 const Impacts = () => (
   <div>
-    <h2 className='text-lg md:text-xl font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Impacts</h2>
+    <h2 className='text-lg font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Impacts</h2>
     <ul className='list-none'>
       <li className='mb-1'><FontAwesomeIcon icon={faExclamationTriangle} className='mr-2 text-sm' />Intensification of the greenhouse effect</li>
       <li className='mb-1'><FontAwesomeIcon icon={faWater} className='mr-2 text-sm' />Contribution to ocean acidification</li>
@@ -41,7 +41,7 @@ const Impacts = () => (
 
 const Solutions = () => (
   <div>
-    <h2 className='text-lg md:text-xl font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Solutions</h2>
+    <h2 className='text-lg font-semibold mb-1 text-blue-900 dark:text-cyan-400'>Solutions</h2>
     <ul className='list-none'>
       <li className='mb-1'><FontAwesomeIcon icon={faWind} className='mr-2 text-sm' />Limit methane emissions through environmental policies</li>
       <li className='mb-1'><FontAwesomeIcon icon={faFlask} className='mr-2 text-sm' />Adopt methane capture and storage technologies</li>
@@ -86,17 +86,28 @@ export const Methane = () => {
 
   return (
     <div className="md:h-[calc(100vh-56px-2rem)] flex flex-col items-center bg-sky-100 dark:bg-sky-900 rounded p-6 ml-2 mr-2 md:mr-4 shadow-md dark:shadow-slate-900 text-sm md:text-base">
-      <h1 className="text-4xl md:text-5xl font-bold mb-5 text-blue-900 dark:text-cyan-400">Methane</h1>
-      <ul className="prose max-w-screen-lg">
+      <h1 className="text-4xl font-bold mb-4 text-blue-900 dark:text-cyan-400">Methane</h1>
+
+      <div className="prose max-w-screen-lg">
         <p>
-          Methane is one of the primary greenhouse gases in the atmosphere. Even though it is present in smaller amounts compared to CO2, it has a much greater warming potential. The main sources of methane emissions are agriculture, the production and transportation of coal, oil, and gas, and the decomposition of organic waste in disposal sites.
+          The second anthropogenic greenhouse gas, and the most abundant after carbon dioxide, methane accounts for about 20% of global emissions, significantly impacting Earth{"'"}s temperature and the climate system.
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+
+        <p className='my-2'>
+          Although methane is far less abundant in the atmosphere compared to CO2, it has a much greater warming potential.
+        </p>
+
+        <p>
+          The main sources of methane emissions are agriculture, the production and transport of coal, oil, and gas, and the decomposition of organic waste in disposal sites.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
           <Impacts />
           <Solutions />
         </div>
-      </ul>
-      <div className="bg-white dark:bg-gray-100 rounded-md shadow-lg w-full max-w-4xl mt-5 p-5">
+      </div>
+      
+      <div className="bg-white dark:bg-gray-100 rounded-md shadow-lg w-full max-w-4xl mt-4 p-3">
         <LineChart chartData={methaneData} />
       </div>
     </div>
